2057725d91baacc1d5708ddd718aebf7e10ed7e4
Kurapik3 <jimwilliam2005@gmail.com>
2025-11-25 22:49:11 +0800

    Updated all asset and tutorial functions
    please no merge conflict

--- Assets/Scripts/14Tutorial/TutorialUI.cs
+++ Assets/Scripts/14Tutorial/TutorialUI.cs
@@ -31,7 +31,30 @@ public class TutorialUI : MonoBehaviour
                 icon.sprite = TechTree;
                 Text.text = "Research Fishing in tech Tree and Upgrade your tree base";
                 break;
-
+            case TutorialStage.UnlockFishing:
+                icon.sprite = UnlockFishing;
+                Text.text = "Extract the Fish to upgrade your tree base";
+                break;
+            case TutorialStage.TapTree:
+                icon.sprite = TapTree;
+                Text.text = "Tap on the Tree Base";
+                break;
+            case TutorialStage.BuildUnit:
+                icon.sprite = BuildUnit;
+                Text.text = "Train a Builder Unit";
+                break;
+            case TutorialStage.Endturn:
+                icon.sprite = Endturn;
+                Text.text = "Now End Your Turn";
+                break;
+            case TutorialStage.MoveUnit:
+                icon.sprite = MoveUnit;
+                Text.text = "Move your Unit to reveal some fog";
+                break;
+            case TutorialStage.WowGrove:
+                icon.sprite = WowGrove;
+                Text.text = " Wow, a grove! Send Builder to build base. ";
+                break;
         }
     }
 }
