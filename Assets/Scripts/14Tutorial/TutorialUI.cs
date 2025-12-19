46ee35bacd58deee8605c035045af89af63293a6
Kurapik3 <jimwilliam2005@gmail.com>
2025-12-19 17:20:20 +0800

    tutorial

--- Assets/Scripts/14Tutorial/TutorialUI.cs
+++ Assets/Scripts/14Tutorial/TutorialUI.cs
@@ -8,6 +8,8 @@ public class TutorialUI : MonoBehaviour
     [Header("UI Elements")]
     [SerializeField] private Image icon;
     [SerializeField] private TextMeshProUGUI Text;
+    [SerializeField] private Button end;
+    [SerializeField] private Button tribe;
 
     [Header("Icons")]
     [SerializeField] private Sprite TechTree;
@@ -31,30 +33,44 @@ public class TutorialUI : MonoBehaviour
             case TutorialStage.TechTree:
                 icon.sprite = TechTree;
                 Text.text = "Research Fishing in tech Tree and Upgrade your tree base";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
             case TutorialStage.UnlockFishing:
                 icon.sprite = UnlockFishing;
                 Text.text = "Extract the Fish to upgrade your tree base";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
             case TutorialStage.TapTree:
                 icon.sprite = TapTree;
                 Text.text = "Tap on the Tree Base";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
             case TutorialStage.BuildUnit:
                 icon.sprite = BuildUnit;
                 Text.text = "Train a Builder Unit";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
             case TutorialStage.Endturn:
                 icon.sprite = Endturn;
                 Text.text = "Now End Your Turn";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
             case TutorialStage.MoveUnit:
                 icon.sprite = MoveUnit;
                 Text.text = "Move your Unit to reveal some fog";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
             case TutorialStage.WowGrove:
                 icon.sprite = WowGrove;
                 Text.text = "Wow, a grove! Send Builder to build base. ";
+                end.interactable = false;
+                tribe.interactable = false;
                 break;
         }
     }
